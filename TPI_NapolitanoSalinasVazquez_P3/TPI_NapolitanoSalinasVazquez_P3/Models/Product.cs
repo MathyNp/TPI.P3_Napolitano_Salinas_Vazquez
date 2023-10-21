f7156df86032d0d9ef2f333ddf991058db383189
Ariel <salinas.ariel02@gmail.com>
2023-10-21 15:53:05 -0300

    sqlite config

--- TPI_NapolitanoSalinasVazquez_P3/TPI_NapolitanoSalinasVazquez_P3/Models/Product.cs
+++ TPI_NapolitanoSalinasVazquez_P3/TPI_NapolitanoSalinasVazquez_P3/Models/Product.cs
@@ -1,8 +1,13 @@
-﻿namespace TPI_NapolitanoSalinasVazquez_P3.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TPI_NapolitanoSalinasVazquez_P3.Models
 {
     public class Product
     {
-        private int productId { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        private int Id { get; set; }
         public string productName { get; set; }
         public int productPrice { get; set; }
         public int productStock { get; set; }
